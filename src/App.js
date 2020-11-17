595bcf5a374f6946f89f0c3071cfb8cc893351c3
n7231 <itaniaanni@gmail.com>
2020-11-17 16:06:22 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [jackets, setJackets] = useState([]);
+
+  useEffect(() => {
+    
+  fetch('https://bad-api-assignment.reaktor.com/products/jackets')
+  .then(response => response.json())
+  .then(data => setJackets(data));
+  }
+  )
+console.log("jackets", jackets)
+
+if (!jackets) {
+  return <></>
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <div style={{ borderRadious:'25px', border: '2px solid red'}}>
+
+      {/*<div style={{ margin:'2px'}}>
+          {jackets[0].name}
+        </div>
+        
+        <div style={{ margin:'2px'}}>
+          {jackets[0].price}
+        </div>
+        
+        <div style={{ margin:'2px'}}>
+          {jackets[0].type}
+        </div>
+  </div>*/}
+  </div>
+
+      {jackets.map((jacket, index) => 
+         <div key={index}
+         style={{ margin:'2px' , borderRadious:'25px', border: '2px solid red'}}>
+          <div style={{ margin:'2px'}}>
+            {jacket.price}
+          </div>
+          <div style={{ margin:'2px'}}>
+            {jacket.name}
+          </div>
+          <div style={{ margin:'2px'}}>
+            {jacket.color[0]}
+          </div>
+
+      
+      </div>
+    )}
+
+
     </div>
   );
 }
